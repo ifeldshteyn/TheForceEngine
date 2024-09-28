e92a0c9a8a56593b75193df84560931c4084aeb9
Karjala22 <78927981+Karjala22@users.noreply.github.com>
2024-09-28 16:13:09 -0400

    Adding timestamps to log file

--- TheForceEngine/TFE_System/log.cpp
+++ TheForceEngine/TFE_System/log.cpp
@@ -9,10 +9,12 @@
 #include <assert.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctime>
+#include <chrono>
 
 #ifdef _WIN32
-	#include <Windows.h>
-	#include <io.h>
+#include <Windows.h>
+#include <io.h>
 #endif
 
 namespace TFE_System
@@ -20,7 +22,7 @@ namespace TFE_System
 	static FileStream s_logFile;
 	static char s_workStr[32768];
 	static char s_msgStr[32768];
-	static const char* c_typeNames[]=
+	static const char* c_typeNames[] =
 	{
 		"",			//LOG_MSG = 0,
 		"Warning",	//LOG_WARNING,
@@ -40,7 +42,7 @@ namespace TFE_System
 	{
 		s_logFile.close();
 	}
-	
+
 	void debugWrite(const char* tag, const char* str, ...)
 	{
 		if (!tag || !str) { return; }
@@ -54,16 +56,24 @@ namespace TFE_System
 		sprintf(s_workStr, "[%s] %s\r\n", tag, s_msgStr);
 
 		//Write to the debugger or terminal output.
-		#ifdef _WIN32
-			OutputDebugStringA(s_workStr);
-		#else
-			fprintf(stderr, "%s", s_workStr);
-		#endif
+#ifdef _WIN32
+		OutputDebugStringA(s_workStr);
+#else
+		fprintf(stderr, "%s", s_workStr);
+#endif
 	}
 
 	void logWrite(LogWriteType type, const char* tag, const char* str, ...)
 	{
 		if (type >= LOG_COUNT || !s_logFile.isOpen() || !tag || !str) { return; }
+
+		auto now = std::chrono::system_clock::now();
+		std::time_t now_c = std::chrono::system_clock::to_time_t(now);
+		std::tm now_tm;
+		localtime_s(&now_tm, &now_c);  // For thread safety on Windows
+
+		char timeStr[32];
+		strftime(timeStr, sizeof(timeStr), "%Y-%b-%d %H:%M:%S", &now_tm);
 
 		//Handle the variable input, "printf" style messages
 		va_list arg;
@@ -73,11 +83,11 @@ namespace TFE_System
 		//Format the message
 		if (type != LOG_MSG)
 		{
-			sprintf(s_workStr, "[%s : %s] %s\r\n", c_typeNames[type], tag, s_msgStr);
+			sprintf(s_workStr, "%s - [%s : %s] %s\r\n", timeStr, c_typeNames[type], tag, s_msgStr);
 		}
 		else
 		{
-			sprintf(s_workStr, "[%s] %s\r\n", tag, s_msgStr);
+			sprintf(s_workStr, "%s - [%s] %s\r\n", timeStr, tag, s_msgStr);
 		}
 		//Write to disk
 		s_logFile.writeBuffer(s_workStr, (u32)strlen(s_workStr));
@@ -87,18 +97,18 @@ namespace TFE_System
 			s_logFile.flush();
 		}
 		//Write to the debugger or terminal output.
-		#ifdef _WIN32
-			OutputDebugStringA(s_workStr);
-		#else
-			fprintf(stderr, "%s", s_workStr);
-		#endif
+#ifdef _WIN32
+		OutputDebugStringA(s_workStr);
+#else
+		fprintf(stderr, "%s", s_workStr);
+#endif
 		//Critical log messages also act as asserts in the debugger.
 		if (type == LOG_CRITICAL)
 		{
 			assert(0);
 		}
 
-		sprintf(s_workStr, "[%s] %s", tag, s_msgStr);
+		sprintf(s_workStr, "%s - [%s] %s", timeStr, tag, s_msgStr);
 		size_t len = strlen(s_msgStr);
 		char* msg = s_msgStr;
 		char* msgStart = msg;
